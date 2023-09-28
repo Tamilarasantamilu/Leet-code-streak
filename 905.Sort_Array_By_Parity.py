719ff2c148a8845b0742466023a8df8cb5c6b003
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2023-09-28 18:46:24 +0530

    Create 905.Sort_Array_By_Parity.py

---
+++ 905.Sort_Array_By_Parity.py
@@ -0,0 +1,10 @@
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        res1=[]
+        res2=[]
+        for i in nums:
+            if i%2==0:
+                res1.append(i)
+            else:
+                res2.append(i)
+        return res1+res2
