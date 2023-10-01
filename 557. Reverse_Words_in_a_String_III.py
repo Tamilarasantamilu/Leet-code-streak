84f161151c245f087dfc1446598e80ba2720c24f
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2023-10-01 21:01:38 +0530

    Update 557. Reverse_Words_in_a_String_III.py

--- 557. Reverse_Words_in_a_String_III.py
+++ 557. Reverse_Words_in_a_String_III.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        result = ''
+        str_list = s.split()
+        n = len(str_list)
+        for i in range(n):
+            temp = self.reverseWord(str_list[i])
+            result += temp
+            result += ' '
+        result = result[:len(result) - 1]
+        return result
+    def reverseWord(self, word: str) -> str:
+        return word[::-1]
