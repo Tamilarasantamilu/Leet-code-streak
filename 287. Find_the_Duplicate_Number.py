d0cc27acbd76abdaf968694a79dbdada94606e40
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2023-10-01 21:02:53 +0530

    Update 287. Find_the_Duplicate_Number.py

--- 287. Find_the_Duplicate_Number.py
+++ 287. Find_the_Duplicate_Number.py
@@ -1,13 +1,12 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        # Phase 1: Intersection point
+        # Phase1:Intersection point
         fast = slow = nums[0]
         while True:
             fast = nums[nums[fast]]
             slow = nums[slow]
             if fast == slow:
                 break
-        
         # Phase 2: Find the cycle entrance
         slow = nums[0]
         while slow != fast:
