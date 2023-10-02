7d738a26b2d0f1a71b2191511b1b2fff8ecc0c72
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2023-10-02 22:26:38 +0530

    Update 2038. Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color.py

--- 2038. Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color.py
+++ 2038. Remove_Colored_Pieces_if_Both_Neighbors_are_the_Same_Color.py
@@ -2,7 +2,6 @@ class Solution:
     def winnerOfGame(self, colors: str) -> bool:
         countAAA = 0
         countBBB = 0
-
         for a, b, c in zip(colors, colors[1:], colors[2:]):
             if 'A' == a == b == c:
                 countAAA += 1
