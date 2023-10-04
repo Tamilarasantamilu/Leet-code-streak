7a48f94a40b90e55fd87285b2319c56fbf878b3c
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2023-10-04 22:32:03 +0530

    Update 706.Design_Hashmap.py

--- 706.Design_Hashmap.py
+++ 706.Design_Hashmap.py
@@ -1,1 +1,12 @@
+class MyHashMap:
+    def __init__(self):
+        self.l=[-1 for i in range(1000001)]
+        
+    def put(self, key: int, value: int) -> None:
+        self.l[key]=value
 
+    def get(self, key: int) -> int:
+        return self.l[key]
+
+    def remove(self, key: int) -> None:
+        self.l[key]=-1
