8435240656a754477121ae8111625c19bbeffee6
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2023-10-16 19:16:34 +0530

    Create 119.Pascal's_Triangle_II.py

---
+++ 119.Pascal's_Triangle_II.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+        for i in range(rowIndex):
+            row = [1] + [row[j] + row[j+1] for j in range(i)] + [1]
+        return row
+        
