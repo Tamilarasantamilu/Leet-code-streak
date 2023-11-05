52dcbf28109fefd6ea2c9b090f5936d2992442e0
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2023-11-05 09:49:05 +0530

    Update 229_Majority_Element_II.py

--- 229_Majority_Element_II.py
+++ 229_Majority_Element_II.py
@@ -1,11 +1,9 @@
 from collections import Counter
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        hashmap=Counter(nums)
-        
+        hashmap=Counter(nums)     
         result=[]
         for key,value in hashmap.items():
             if(value>(len(nums)//3)):
                 result.append(key)
         return result
-        
