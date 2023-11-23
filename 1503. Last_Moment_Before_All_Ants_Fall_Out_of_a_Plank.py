619cd8238311467d7308563a93695017d585e534
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2023-11-23 22:19:59 +0530

    Update 1503. Last_Moment_Before_All_Ants_Fall_Out_of_a_Plank.py

--- 1503. Last_Moment_Before_All_Ants_Fall_Out_of_a_Plank.py
+++ 1503. Last_Moment_Before_All_Ants_Fall_Out_of_a_Plank.py
@@ -2,4 +2,3 @@ class Solution:
     def getLastMoment(self, n: int, left: List[int], right: List[int]) -> int:
         farthest = max(max(left,default=0),n-min(right,default=n))
         return farthest
-        
