6aa4253d4e6a241ed225ca2727987bc60c36b31d
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2024-04-09 20:40:40 +0530

    Create 2073. Time_Needed_to_Buy_Tickets.py

---
+++ 2073. Time_Needed_to_Buy_Tickets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        time = 0
+
+        for x in range(len(tickets)):
+            if tickets[x] >= tickets[k]:
+                time += tickets[k]
+            elif tickets[x] < tickets[k]:
+                time += tickets[x]
+
+            if x > k and tickets[x] >= tickets[k]:
+                time -= 1
+
+        return time
+        
