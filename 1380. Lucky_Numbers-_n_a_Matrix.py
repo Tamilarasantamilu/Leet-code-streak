367a9b1380202120dd4dad1dbd036321b57dcea3
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2024-07-19 23:09:29 +0530

    Create 1380. Lucky_Numbers-_n_a_Matrix.py

---
+++ 1380. Lucky_Numbers-_n_a_Matrix.py
@@ -0,0 +1,12 @@
+class Solution:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        m = len(matrix)
+        n = len(matrix[0])
+        lis = []
+        for i in range(m):
+            min_row = min(matrix[i])
+            col_max = matrix[i].index(min_row)
+            max_col = max(matrix[j][col_max] for j in range(m))
+            if min_row == max_col:
+                lis.append(min_row)
+        return lis        
