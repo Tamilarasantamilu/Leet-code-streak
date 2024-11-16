7635eaed25b752bec61c5bb8ac9a8c61c9eb7d25
Tamilarasan S <112767697+Tamilarasantamilu@users.noreply.github.com>
2024-11-16 23:02:33 +0530

    Create 3254. Find_the_Power_of_K-Size_Subarrays_I.py

---
+++ 3254. Find_the_Power_of_K-Size_Subarrays_I.py
@@ -0,0 +1,16 @@
+class Solution:
+    def resultsArray(self, nums: List[int], k: int) -> List[int]:
+        def is_consecutive_and_sorted(subarray):
+            return all(subarray[i] + 1 == subarray[i + 1] for i in range(len(subarray) - 1))
+
+        n = len(nums)
+        result = []
+
+        for i in range(n - k + 1):
+            subarray = nums[i:i + k]
+            if is_consecutive_and_sorted(subarray):
+                result.append(max(subarray))
+            else:
+                result.append(-1)
+        
+        return result
